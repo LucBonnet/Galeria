b7d149f0065b9eaf9128bca9252d3d984ebddd31
LucBonnet <36719698+LucBonnet@users.noreply.github.com>
2020-06-22 02:33:51 -0300

    implementação da api

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const server = express();
 
+const fetch = require('node-fetch');
+require('dotenv/config');
+
 server.use(express.static("public"));
 
 const nunjucks = require('nunjucks');
@@ -9,8 +12,40 @@ nunjucks.configure('src/views', {
     noCache: true
 })
 
+function dataFormatter(data){
+    // console.log(data);
+    // let dia = data.getDay() + '';
+    // let mes = data.getMonth() + '';
+    // let ano = data.getFullYear() + '';
+
+    // dia = dia.length == 1?'0'+dia:dia;
+    // mes = mes.length == 1?'0'+mes:mes;
+    // console.log(`${dia}/${mes}/${ano}`)
+    // return `${dia}/${mes}/${ano}`;
+
+    let dia = data.substring(8, 10);
+    let mes = data.substring(5, 7);
+    let ano = data.substring(0, 4);
+    let formattedDate = dia + '/' + mes + '/' + ano;
+
+    return formattedDate;
+}
+
 server.get("/", (req, res) => {
-    return res.render("index.html");
+    const url = `https://api.unsplash.com/photos/random?client_id=${process.env.ACCESS_TOKEN}&orientation=landscape&count=12`;
+
+    fetch(url)
+    .then( res => res.json() )
+    .then( images => {
+        for(image of images){
+            //const data = new Date(image.created_at);
+            image.created_at = dataFormatter(image.created_at);
+        }
+
+        return res.render("index.html", { images: images });
+    })
 })
+
+
 
 server.listen(3000);
