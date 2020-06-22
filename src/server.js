9adb9a35e4633616b1c91ad7146e7bfee666b3af
LucBonnet <36719698+LucBonnet@users.noreply.github.com>
2020-06-22 00:50:47 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const server = express();
+
+server.use(express.static("public"));
+
+const nunjucks = require('nunjucks');
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+})
+
+server.get("/", (req, res) => {
+    return res.render("index.html");
+})
+
+server.listen(3000);
